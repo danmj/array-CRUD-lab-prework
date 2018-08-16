2ca79353f52704526dd3ec5760ca8862cb058aa8
danmj <daniel.jung@stern.nyu.edu>
2018-08-16 13:16:05 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  characters.push("Squidward")
+  characters.push("Mr. Krabs")
 end
 
 def add_element_to_start_of_array(array, element)
