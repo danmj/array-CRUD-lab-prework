1ce9771ae2282a9c00b07df148372516838e8f34
danmj <daniel.jung@stern.nyu.edu>
2018-08-16 13:15:30 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  characters = ["Spongebob", "Patrick", "Gary", "Squidward"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  characters.push
 end
 
 def add_element_to_start_of_array(array, element)
